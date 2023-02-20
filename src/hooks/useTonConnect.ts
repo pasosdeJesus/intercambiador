b406df17451b6652ce353af7ed1881b8bc754c11
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-02-19 22:11:10 -0500

    Une js y rails

---
+++ src/hooks/useTonConnect.ts
@@ -0,0 +1,37 @@
+import { useTonConnectUI } from '@tonconnect/ui-react';
+import { Sender, SenderArguments } from 'ton-core';
+
+export function useTonConnect(): { sender: Sender; connected: boolean, depura: any, reporta: any} {
+  const [tonConnectUI] = useTonConnectUI();
+
+  return {
+    sender: {
+      send: async (args: SenderArguments) => {
+        tonConnectUI.sendTransaction({
+          messages: [
+            {
+              address: args.to.toString(),
+              amount: args.value.toString(),
+              payload: args.body?.toBoc().toString('base64'),
+            },
+          ],
+          validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
+        });
+      },
+    },
+    connected: tonConnectUI.connected,
+    depura: async () => {
+	    const walletsList = await tonConnectUI.connector.getWallets();
+	    debugger
+    },
+    reporta: () => {
+	    tonConnectUI.connector.onStatusChange(
+		    walletInfo => {
+			    // Llamada (varias veces) cuando se conecta o
+			    // desconecta
+			    console.log('Connection status:', walletInfo);
+		    }
+	    )
+    },
+  };
+}
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+const manifestUrl = 'https://copton.pasosdeJesus.org/tonconnect-manifest.json';
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
