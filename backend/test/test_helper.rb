d1d1720dccca5e371d6d69d07bb62869fce4ffc3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-02-26 06:58:42 -0500

    test and sample data

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -6,16 +6,34 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-
   # Add more helper methods to be used by all tests here...
 end
 
+# To reduce problems with fixtures we leave examples in this file
+
 PRUEBA_ANUNCIO = {
-  personasaprox: 100,
-  organizacionarmada: 1,
+  ton: 10,
+  margenflotante: 102,
+  limiteinferior: 5,
+  maximotiempo: 100,
+  usuario_id: 1,
+  created_at: '2023-02-25',
+  updated_at: '2023-02-25',
 }
 
 
+PRUEBA_USUARIO = {
+  nusuario: "juan_perez",
+  password: "sjrven123",
+  nombre: "Juan Perez",
+  descripcion: "Juan Perez",
+  rol: 5,
+  idioma: "es_CO",
+  email: "juanperez@localhost",
+  encrypted_password: '$2a$10$uMAciEcJuUXDnpelfSH6He7BxW0yBeq6VMemlWc5xEl6NZRDYVA3G',
+  sign_in_count: 0,
+  fechacreacion: '2023-02-25',
+  fechadeshabilitacion: nil,
+  created_at: '2023-02-25',
+  updated_at: '2023-02-25',
+}
