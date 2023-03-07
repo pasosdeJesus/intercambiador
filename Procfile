1d847f28a406293b13ec6e54d8d0c0e6629ac299
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-03-06 21:46:26 -0500

    Shares secret for JWT between the two backends

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 rails: (cd backend; export R=f; bin/corre)
+go: (cd backend-auth; export PORT=8081; ./tonproof)
 js: (cd frontend; yarn dev --host --debug)
 
--- bin/run.sh
+++ bin/run.sh
@@ -7,5 +7,12 @@ if (test ! -f .env) then {
 
 . .env
 
+# According to
+# https://security.stackexchange.com/questions/262106/crack-jwt-hs256-with-hashcat
+# HS256 is secure (hard to find the secret) when the secret has more
+# than 128 bits of entropy. We choose 256 bits randonmly
+export HS256_SECRET=`od -vAn -N32 -ta < /dev/urandom | tr -d ' ' | tr -d '\n'`
+echo "HS256_SECRET: ${HS256_SECRET}"
+
 foreman start -f Procfile
 
